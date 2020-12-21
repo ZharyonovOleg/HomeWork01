0f117f8d477cb332d01dead0fea13df080757486
ZharyonovOleg <zharyonovoleg@gmail.com>
2020-12-21 23:16:10 +0500

    update 2 in Task_1.1.py

--- Task_1.1.py
+++ Task_1.1.py
@@ -2,6 +2,6 @@ number_01 = 50
 number_02 = 150
 print(number_01, number_02)
 
-number_03 = input('Введите втовое число: ')
+number_03 = input('Введите третье число: ')
 number_04 = input('Введите четвёртое число: ')
 print(number_03, number_04)
