c42258c2a32a85b866eff6604fc586994220e3da
ZharyonovOleg <zharyonovoleg@gmail.com>
2020-12-26 22:00:40 +0500

    Update 2 Task_02-02.py

--- Task_02-02.py
+++ Task_02-02.py
@@ -9,6 +9,14 @@ print(my_list)
 
 
 #
-
+# range не увидел в методичке
+my_list = [input('Введите первый элемент списка: '),
+           input('Введите второй элемент списка: '),
+           input('Введите третий элемент списка: '),
+           input('Введите четвёртый элемент списка: '),
+           input('Введите пятый элемент списка: ')]
+for i in range(1, len(my_list), 2):
+    my_list[i - 1], my_list[i] = my_list[i], my_list[i - 1]
+print(my_list)
 
 
