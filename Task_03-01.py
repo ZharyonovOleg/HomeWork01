d5f149522b81431d03271ddd9ce0a80ff5fe079f
ZharyonovOleg <zharyonovoleg@gmail.com>
2020-12-28 00:29:05 +0500

    Update Task_03-01.py

--- Task_03-01.py
+++ Task_03-01.py
@@ -1,9 +1,6 @@
-def division():
-    num_1 = int(input('Введите числитель: '))
-    num_2 = int(input('Введите знаменатель: '))
+def division(num_1, num_2):
     while num_2 == 0:
         print('Вы ввели для знаменателя значение "ноль", а на ноль делить нельзя!')
         num_2 = int(input('Введите знаменатель: '))
     print('Ответ: ', round((num_1 / num_2), 3))
-
-division()
+division((int(input('Введите числитель: '))), (int(input('Введите знаменатель: '))))
