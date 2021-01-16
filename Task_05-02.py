8c1cc8f479c31234ae67841892e2413cc7f65dd1
ZharyonovOleg <zharyonovoleg@gmail.com>
2021-01-16 18:03:39 +0500

    Create(txt) & update(Task_05-02.py) some files

--- Task_05-02.py
+++ Task_05-02.py
@@ -5,5 +5,5 @@ my_lines = f_obj.readlines()
 print('Количество строк в файле = ', len(my_lines))
 
 for x, y in enumerate(my_lines):
-    print(x, 'строка содержит - ', len(y.split()), 'слов')
+    print((x + 1), 'строка содержит - ', len(y.split()), 'слов')
 f_obj.close()
