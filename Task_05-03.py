cd5c89d0345ece30855609f2352e67dccec65653
ZharyonovOleg <zharyonovoleg@gmail.com>
2021-01-16 19:15:48 +0500

    Update Task_05-03.py

--- Task_05-03.py
+++ Task_05-03.py
@@ -8,7 +8,7 @@ f_obj.close()
 num_empl = len(my_lines)
 
 for el in my_lines:
-    if float(re.sub(r'\D', "", el[el.find(' ') + 1:])) > 20000: # решено с помощью гугла re sub \D
+    if float(re.sub(r'\D', "", el[el.find(' ') + 1:])) > 20000: # решено с помощью гугла re sub \D, НЕ ПОНЯЛ КАК ИЗБАВИТЬСЯ ОТ \n в конце
         print(el[:el.find(' ')], 'зарабатывает больше 20000 рублей')
 
 print('Количество сотрудников, информация о которых содержится в данном файле составляет: ', num_empl)
