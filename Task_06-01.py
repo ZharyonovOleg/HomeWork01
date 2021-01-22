c202d14eff1edfd840a4e8656405c9a503a89638
ZharyonovOleg <zharyonovoleg@gmail.com>
2021-01-22 19:24:27 +0500

    Update Task_06-01.py

--- Task_06-01.py
+++ Task_06-01.py
@@ -2,15 +2,15 @@ import time
 
 class TrafficLight:
     def __init__(self, color):
-        self._color = color
+        self.__color = color
 
         self.running()
     def running(self):
-        print(self._color, 'Red')
+        print(self.__color, 'Red')
         time.sleep(7)
-        print(self._color, 'Yellow')
+        print(self.__color, 'Yellow')
         time.sleep(2)
-        print(self._color, 'Green')
+        print(self.__color, 'Green')
         time.sleep(3)
 
 tr_l = TrafficLight('TrafficLight: ')
