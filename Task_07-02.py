fbd1adcd4759e30d54538e29e785cf6ac3179327
ZharyonovOleg <zharyonovoleg@gmail.com>
2021-01-27 20:56:22 +0500

    Update Task_07-02.py

--- Task_07-02.py
+++ Task_07-02.py
@@ -1,39 +1,87 @@
-from abc import ABC, abstractmethod
+################ Task_07-02.py #######################
 
-class Clothes(ABC):
-    def __init__(self, v, h):
-        self.size = v
-        self.height = h
-
-    @abstractmethod
-    def coat(self):
-        pass
-
-    @abstractmethod
-    def suite(self):
-        pass
-
-class Cloth(Clothes):
-    @property
-    def coat(self):
-        self.coat_cloth = float((float(self.size) / 6.5) + 0.5)
-        return self.coat_cloth
-
-    @property
-    def suite(self):
-        self.suite_cloth = float((float(self.height) * 2) + 0.3)
-        return self.suite_cloth
-
-    @property
-    def total(self):
-        self.total_cloth = self.coat_cloth + self.suite_cloth
-        return self.total_cloth
+'''2. Реализовать проект расчета суммарного расхода ткани на производство
+одежды. Основная сущность (класс) этого проекта — одежда, которая может иметь
+определенное название. К типам одежды в этом проекте относятся пальто и костюм.
+У этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма).
+Это могут быть обычные числа: V и H, соответственно.
+Для определения расхода ткани по каждому типу одежды использовать формулы:
+для пальто (V/6.5 + 0.5), для костюма (2 * H + 0.3). Проверить работу этих 
+методов на реальных данных.
+Реализовать общий подсчет расхода ткани. Проверить на практике полученные 
+на этом уроке знания: реализовать абстрактные классы для основных классов 
+проекта, проверить на практике работу декоратора @property.'''
 
 
-cloth = Cloth(float(input('Введите размер пальто: ')), (float(input('Введите ростовку костюма: '))))
+# from abc import ABC, abstractmethod
+#
+# class Clothes(ABC):
+#     def __init__(self, v, h):
+#         self.size = v
+#         self.height = h
+#
+#     @abstractmethod
+#     def coat(self):
+#         pass
+#
+#     @abstractmethod
+#     def suite(self):
+#         pass
+#
+# class Cloth(Clothes):
+#     @property
+#     def coat(self):
+#         self.coat_cloth = float((float(self.size) / 6.5) + 0.5)
+#         return self.coat_cloth
+#
+#     @property
+#     def suite(self):
+#         self.suite_cloth = float((float(self.height) * 2) + 0.3)
+#         return self.suite_cloth
+#
+#     @property
+#     def total(self):
+#         self.total_cloth = self.coat_cloth + self.suite_cloth
+#         return self.total_cloth
+#
+#
+# cloth = Cloth(float(input('Введите размер пальто: ')), (float(input('Введите ростовку костюма: '))))
+#
+# print('На пальто понадобится ', cloth.coat, 'ткани')
+#
+# print('На костюм понадобится ', cloth.suite, 'ткани')
+#
+# print('На костюм понадобится ', cloth.total, 'ткани')
 
-print('На пальто понадобится ', cloth.coat, 'ткани')
-
-print('На костюм понадобится ', cloth.suite, 'ткани')
-
-print('На костюм понадобится ', cloth.total, 'ткани')
+# ОТ ПРЕПОДАВАТЕЛЯ:
+#
+# from abc import ABC, abstractmethod
+#
+#
+# class Clothes(ABC):
+#     def __init__(self, param):
+#         self.param = param
+#
+#     @abstractmethod
+#     def calculate(self):
+#         pass
+#
+#
+# class Coat(Clothes):
+#
+#     @property
+#     def calculate(self):
+#         return round((self.param / 6.5) + 0.5)
+#
+#
+# class Suit(Clothes):
+#
+#     @property
+#     def calculate(self):
+#         return round((2 * self.param) + 0.3)
+#
+#
+# coat = Coat(45)
+# suit = Suit(170)
+# print(coat.calculate)
+# print(suit.calculate)
